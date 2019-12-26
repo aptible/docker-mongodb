043b2a9dff87b641078c0a89c3c0ba31f57bc929
Alex Kubacki <alex@aptible.com>
2019-12-26 14:39:23 -0500

    Simplify logic now that 2.6 is deprecated.

--- test/assert-votes.js
+++ test/assert-votes.js
@@ -3,11 +3,8 @@ var ret = 0;
 
 for (var i = 0; i < conf["members"].length; i++) {
   member = conf["members"][i];
-  // In Mongo 2.6, priority and votes aren't returned when
-  // they are default (1), so we check for undefined as well.
   if (
-      (member.priority !== 1 && typeof member.priority !== 'undefined') ||
-      (member.votes !== 1 && typeof member.votes !== 'undefined')
+      (member.priority !== 1) || (member.votes !== 1)
   ) {
     print("MISCONFIGURED: " + member.host);
     printjson(member);
