9b9459038c853b89babab1e6d1eb3b4410224943
Alex Kubacki <alex@aptible.com>
2021-02-11 11:15:01 -0700

    Initialize replicas with a lower priority. (#47)

--- test/assert-votes.js
+++ test/assert-votes.js
@@ -1,10 +1,22 @@
 var conf = rs.conf();
 var ret = 0;
+var desired_priority = 0;
 
 for (var i = 0; i < conf["members"].length; i++) {
+
   member = conf["members"][i];
+
+  switch(member.host) {
+    case "mongodb-r1:27117":
+      desired_priority = 1;
+      break;
+    default:
+      desired_priority = 0.5;
+  }
+
+
   if (
-      (member.priority !== 1) || (member.votes !== 1)
+      (member.priority !== desired_priority) || (member.votes !== 1)
   ) {
     print("MISCONFIGURED: " + member.host);
     printjson(member);
